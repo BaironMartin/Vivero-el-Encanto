db83d60d0e0a5876fda06785d3f96b90d6668ec9
Bairon Martin <baironmartin2020@hotmail.com>
2024-11-15 20:33:48 -0500

    v1

---
+++ js/app.js
@@ -0,0 +1,65 @@
+window.addEventListener("load", () => {
+  document.querySelector(".menu-btn").addEventListener("click", () => {
+      document.querySelector(".parent-link").classList.toggle("show");
+  });
+});
+
+
+
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next() {
+  let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function(){
+    slider.style.transition = "none";
+    slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+function Prev() {
+  let sliderSection = document.querySelectorAll(".slider__section");
+  let sliderSectionLast = sliderSection[sliderSection.length -1];
+  slider.style.marginLeft = "0";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function(){
+    slider.style.transition = "none";
+    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+btnRight.addEventListener('click', function(){
+  Next();
+});
+
+btnLeft.addEventListener('click', function(){
+  Prev();
+});
+
+setInterval(function(){
+  Next();
+}, 20000);
+
+
+let previusTitle = document.title;
+function changeTitle() {
+    document.title = "¡No te vayas 😅!";
+}
+
+function restoreTitle() {
+    document.title = previusTitle; 
+}
+
+window.addEventListener('blur',changeTitle);
+
+window.addEventListener('focus', restoreTitle);
